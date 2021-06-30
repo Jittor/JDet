e5d423d42cc9457201c4d7a5010af5890af19184
cxjyxx_me <498731903@qq.com>
2021-06-30 22:17:30 +0800

    anchor generator

--- python/jdet/models/boxes/anchor_generator.py
+++ python/jdet/models/boxes/anchor_generator.py
@@ -1,14 +1,21 @@
 import jittor as jt 
 import numpy as np
+from jdet.utils.registry import MODELS
 
+
+@MODELS.register_module()
 class AnchorGeneratorRotated:
-    def __init__(self, base_size, scales, ratios, angles=[0,],scale_major=True, ctr=None):
-        self.base_size = base_size
-        self.scales = jt.array(scales)
+    def __init__(self, strides, ratios, scales, base_sizes=None, angles=[0,],scale_major=True, centers=None, center_offset=0.5, mode='H'):
+        self.strides = jt.array(strides) #TODO support [h, w] stride
         self.ratios = jt.array(ratios)
-        self.angles = jt.array(angles)
+        self.scales = jt.array(scales)
+        self.base_sizes = [stride for stride in self.strides] if base_sizes is None else base_sizes
+        assert(mode in ['H', 'R'])
+        self.mode = mode
+        self.angles = jt.array(angles) if self.mode == 'R' else jt.array([0.])
         self.scale_major = scale_major
-        self.ctr = ctr
+        self.centers = centers
+        self.center_offset = center_offset
         self.base_anchors = self.gen_base_anchors()
 
     @property
@@ -17,31 +24,31 @@ class AnchorGeneratorRotated:
 
     @property
     def num_levels(self):
-        return len(self.ratios)
+        return len(self.strides)
 
     def gen_base_anchors(self):
         multi_level_base_anchors = []
         for i, base_size in enumerate(self.base_sizes):
-            ctr = None
-            if self.ctr is not None:
-                ctr = self.ctr[i]
+            centers = None
+            if self.centers is not None:
+                centers = self.centers[i]
             multi_level_base_anchors.append(
                 self.gen_single_level_base_anchors(
                     base_size,
                     scales=self.scales,
                     ratios=self.ratios,
                     angles=self.angles,
-                    ctr=ctr))
+                    centers=centers))
         return multi_level_base_anchors
 
-    def gen_single_level_base_anchors(self, base_size, scales, ratios, angles, ctr):
+    def gen_single_level_base_anchors(self, base_size, scales, ratios, angles, centers):
         w = base_size
         h = base_size
-        if ctr is None:
-            x_ctr = 0.5 * (w - 1)
-            y_ctr = 0.5 * (h - 1)
+        if centers is None:
+            x_ctr = self.center_offset * (w - 1)
+            y_ctr = self.center_offset * (h - 1)
         else:
-            x_ctr, y_ctr = ctr
+            x_ctr, y_ctr = centers
 
         h_ratios = jt.sqrt(ratios)
         w_ratios = 1 / h_ratios
@@ -57,9 +64,14 @@ class AnchorGeneratorRotated:
         # pixel center
         x_ctr += jt.zeros_like(ws)
         y_ctr += jt.zeros_like(ws)
-        base_anchors = jt.stack(
-            [x_ctr, y_ctr, ws, hs, angles], dim=-1)
-
+        if (self.mode == 'H'):
+            base_anchors = jt.stack(
+                [x_ctr - 0.5 * ws, y_ctr - 0.5 * hs, 
+                 x_ctr + 0.5 * ws, y_ctr + 0.5 * hs], dim=-1)
+        else:
+            base_anchors = jt.stack(
+                [x_ctr - 0.5 * ws, y_ctr - 0.5 * hs, 
+                 x_ctr + 0.5 * ws, y_ctr + 0.5 * hs, angles], dim=-1)
         return base_anchors
 
     def _meshgrid(self, x, y, row_major=True):
@@ -89,15 +101,22 @@ class AnchorGeneratorRotated:
         shift_y = jt.arange(0, feat_h) * stride
         shift_xx, shift_yy = self._meshgrid(shift_x, shift_y)
         shift_others = jt.zeros_like(shift_xx)
-        shifts = jt.stack(
-            [shift_xx, shift_yy, shift_others, shift_others, shift_others], dim=-1)
+        if (self.mode == 'H'):
+            shifts = jt.stack(
+                [shift_xx, shift_yy, shift_xx, shift_yy], dim=-1)
+        else:
+            shifts = jt.stack(
+                [shift_xx, shift_yy, shift_xx, shift_yy, shift_others], dim=-1)
         shifts = shifts.cast(base_anchor.dtype)
         # first feat_w elements correspond to the first row of shifts
         # add A anchors (1, A, 5) to K shifts (K, 1, 5) to get
         # shifted anchors (K, A, 5), reshape to (K*A, 5)
 
         all_anchors = base_anchor[None, :, :] + shifts[:, None, :]
-        all_anchors = all_anchors.view(-1, 5)
+        if (self.mode == 'H'):
+            all_anchors = all_anchors.view(-1, 4)
+        else:
+            all_anchors = all_anchors.view(-1, 5)
         # first A rows correspond to A anchors of (0, 0) in feature map,
         # then (0, 1), (0, 2), ...
         return all_anchors
@@ -115,136 +134,50 @@ class AnchorGeneratorRotated:
         valid = valid[:, None].expand((valid.size(0), self.num_base_anchors)).view(-1)
         return valid
 
+@MODELS.register_module()
+class AnchorGeneratorYangXue(AnchorGeneratorRotated):
+    def __init__(self, yx_base_size, **kwargs):
+        # TODO: do not use yx_base_size
+        self.yx_base_size = yx_base_size
+        super(AnchorGeneratorYangXue, self).__init__(**kwargs)
 
-class AnchorGenerator:
-    """
-    Examples:
-        >>> self = AnchorGenerator(9, [1.], [1.])
-        >>> all_anchors = self.grid_anchors((2, 2))
-        >>> print(all_anchors)
-        tensor([[ 0.,  0.,  8.,  8.],
-                [16.,  0., 24.,  8.],
-                [ 0., 16.,  8., 24.],
-                [16., 16., 24., 24.]])
-    """
-
-    def __init__(self, base_size, scales, ratios, scale_major=True, ctr=None):
-        self.base_size = base_size
-        self.scales = jt.array(scales)
-        self.ratios = jt.array(ratios)
-        self.scale_major = scale_major
-        self.ctr = ctr
-        self.base_anchors = self.gen_base_anchors()
-
-    @property
-    def num_base_anchors(self):
-        return [base_anchors.size(0) for base_anchors in self.base_anchors]
-
-    @property
-    def num_levels(self):
-        return len(self.ratios)
-
-    def gen_base_anchors(self):
-        multi_level_base_anchors = []
-        for i, base_size in enumerate(self.base_sizes):
-            ctr = None
-            if self.ctr is not None:
-                ctr = self.ctr[i]
-            multi_level_base_anchors.append(
-                self.gen_single_level_base_anchors(
-                    base_size,
-                    scales=self.scales,
-                    ratios=self.ratios,
-                    ctr=ctr))
-        return multi_level_base_anchors
-
-    def gen_single_level_base_anchors(self,
-                                      base_size,
-                                      scales,
-                                      ratios,
-                                      ctr=None):
+    def gen_single_level_base_anchors(self, base_size, scales, ratios, angles, centers):
         w = base_size
         h = base_size
-        if ctr is None:
-            x_ctr = 0.5 * (w - 1)
-            y_ctr = 0.5 * (h - 1)
+        if centers is None:
+            x_ctr = self.center_offset * (self.yx_base_size - 1)
+            y_ctr = self.center_offset * (self.yx_base_size - 1)
         else:
-            x_ctr, y_ctr = ctr
+            x_ctr, y_ctr = centers
 
         h_ratios = jt.sqrt(ratios)
         w_ratios = 1 / h_ratios
-        if self.scale_major:
-            ws = (w * w_ratios[:, None] * scales[None, :]).view(-1)
-            hs = (h * h_ratios[:, None] * scales[None, :]).view(-1)
+        assert self.scale_major, "AnchorGeneratorRotated only support scale-major anchors!"
+
+        ws = np.round(w * w_ratios[:, None, None] / base_size * self.yx_base_size)
+        hs = np.round(ws * ratios[:, None, None])
+        ws = (ws / self.yx_base_size * base_size * scales[None, :, None] *
+            jt.ones_like(angles)[None, None, :]).view(-1)
+        # hs = np.round(h * h_ratios[:, None, None])
+        hs = (hs / self.yx_base_size * base_size * scales[None, :, None] *
+            jt.ones_like(angles)[None, None, :]).view(-1)
+        angles = angles.repeat(len(scales) * len(ratios))
+
+        # use float anchor and the anchor's center is aligned with the
+        # pixel center
+        x_ctr += jt.zeros_like(ws)
+        y_ctr += jt.zeros_like(ws)
+        if (self.mode == 'H'):
+            base_anchors = jt.stack(
+                [x_ctr - 0.5 * ws, y_ctr - 0.5 * hs, 
+                 x_ctr + 0.5 * ws, y_ctr + 0.5 * hs], dim=-1)
         else:
-            ws = (w * scales[:, None] * w_ratios[None, :]).view(-1)
-            hs = (h * scales[:, None] * h_ratios[None, :]).view(-1)
-
-        # yapf: disable
-        base_anchors = jt.stack(
-            [
-                x_ctr - 0.5 * (ws - 1), y_ctr - 0.5 * (hs - 1),
-                x_ctr + 0.5 * (ws - 1), y_ctr + 0.5 * (hs - 1)
-            ],
-            dim=-1).round()
-        # yapf: enable
-
+            base_anchors = jt.stack(
+                [x_ctr - 0.5 * ws, y_ctr - 0.5 * hs, 
+                 x_ctr + 0.5 * ws, y_ctr + 0.5 * hs, angles], dim=-1)
         return base_anchors
 
-    def _meshgrid(self, x, y, row_major=True):
-        xx = x.repeat(len(y))
-        yy = y.view(-1, 1).repeat(1, len(x)).view(-1)
-        if row_major:
-            return xx, yy
-        else:
-            return yy, xx
-
-    def grid_anchors(self, featmap_sizes):
-        assert self.num_levels == len(featmap_sizes)
-        multi_level_anchors = []
-        for i in range(self.num_levels):
-            anchors = self.single_level_grid_anchors(
-                self.base_anchors[i],
-                featmap_sizes[i],
-                self.strides[i])
-            multi_level_anchors.append(anchors)
-        return multi_level_anchors
-
-    def single_level_grid_anchors(self, base_anchor, featmap_size, stride=16):
-        # featmap_size*stride project it to original area
-
-        feat_h, feat_w = featmap_size
-        shift_x = jt.arange(0, feat_w) * stride
-        shift_y = jt.arange(0, feat_h) * stride
-        shift_xx, shift_yy = self._meshgrid(shift_x, shift_y)
-        shifts = jt.stack([shift_xx, shift_yy, shift_xx, shift_yy], dim=-1)
-        shifts = shifts.cast(base_anchor.dtype)
-        # first feat_w elements correspond to the first row of shifts
-        # add A anchors (1, A, 4) to K shifts (K, 1, 4) to get
-        # shifted anchors (K, A, 4), reshape to (K*A, 4)
-
-        all_anchors = base_anchor[None, :, :] + shifts[:, None, :]
-        all_anchors = all_anchors.view(-1, 4)
-        # first A rows correspond to A anchors of (0, 0) in feature map,
-        # then (0, 1), (0, 2), ...
-        return all_anchors
-
-    def valid_flags(self, featmap_size, valid_size):
-        feat_h, feat_w = featmap_size
-        valid_h, valid_w = valid_size
-        assert valid_h <= feat_h and valid_w <= feat_w
-        valid_x = jt.zeros((feat_w,)).bool()
-        valid_y = jt.zeros((feat_h,)).bool()
-        valid_x[:valid_w] = 1
-        valid_y[:valid_h] = 1
-        valid_xx, valid_yy = self._meshgrid(valid_x, valid_y)
-        valid = valid_xx & valid_yy
-        valid = valid[:, None].expand(
-            valid.size(0), self.num_base_anchors).view(-1)
-        return valid
-
-
-class SSDAnchorGenerator(AnchorGenerator):
+class SSDAnchorGenerator(AnchorGeneratorRotated):
     def __init__(self,
                  strides,
                  ratios,
