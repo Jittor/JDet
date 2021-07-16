bba16eed8d3a1505ea9e8e5d888a0519a015042f
li-xl <1905692338@qq.com>
2021-07-16 14:31:47 +0800

    update config

--- configs/s2anet_r50_fpn_1x_dota.py
+++ configs/s2anet_r50_fpn_1x_dota.py
@@ -125,8 +125,8 @@ dataset = dict(
     # ),
     test=dict(
         type="ImageDataset",
-        img_files='/mnt/disk/lxl/dataset/DOTA_1024/test_split/test1024.pkl',
-        img_prefix='/mnt/disk/lxl/dataset/DOTA_1024/test_split/images/',
+        images_file='/mnt/disk/lxl/dataset/DOTA_1024/test_split/test1024.pkl',
+        images_dir='/mnt/disk/lxl/dataset/DOTA_1024/test_split/images/',
         transforms=[
             dict(
                 type="RotatedResize",
