39de14c5e539be966fcc79cd5be23e90852cf5ca
cxjyxx_me <498731903@qq.com>
2021-07-20 15:18:37 +0800

    fix

--- projects/retinanet/check_grad.py
+++ projects/retinanet/check_grad.py
@@ -6,7 +6,7 @@ reload_weights = False
 # reload_weights = True
 
 run_ours = False
-run_ours = True
+# run_ours = True
 
 def check_diff(w1_, w2_):
     w1 = pk.load(open(w1_, "rb"))['model']
@@ -22,6 +22,7 @@ def check_diff(w1_, w2_):
         print(f'{k:45}{abs_err:10.5f}{rel_err:10.5f}{our_err:20.5f}')
 
 def main():
+    model = "retinanet_12"
     if (reload_weights):
         weights_path = '/mnt/disk/cxjyxx_me/JAD/RotationDetection/output/trained_weights/RetinaNet_DOTA_2x_20200915'
         init_name = 'DOTA_initmodel.ckpt'
@@ -33,13 +34,13 @@ def main():
         os.system(f"python convert_weights_tf2jittor.py {os.path.join(weights_path, one_iter_name)} cache_check/i1.pk")
         os.system(f"python weights_cvt.py cache_check/i1.pk")
     if (run_ours):
-        os.system(f"CUDA_VISIBLE_DEVICES=3 python run_net.py --config-file=configs/retinanet_4.py --use_cuda --task=train")
+        os.system(f"gopt_disable=1 CUDA_VISIBLE_DEVICES=2 python run_net.py --config-file=configs/{model}.py --use_cuda --task=train")
 
-    ours_weights_path = '/mnt/disk/cxjyxx_me/JAD/JDet/projects/retinanet/exp/retinanet_4/checkpoints/ckpt_1.pkl'
+    ours_weights_path = f'/mnt/disk/cxjyxx_me/JAD/JDet/projects/retinanet/exp/{model}/checkpoints/ckpt_1.pkl'
     yx_weights_path = 'cache_check/i1.pk_jt.pk'
     check_diff(ours_weights_path, yx_weights_path)
 
-    # ours_weights_path = '/mnt/disk/cxjyxx_me/JAD/JDet/projects/retinanet/exp/retinanet_4/checkpoints/ckpt_0.pkl'
+    # ours_weights_path = f'/mnt/disk/cxjyxx_me/JAD/JDet/projects/retinanet/exp/{model}/checkpoints/ckpt_0.pkl'
     # yx_weights_path = 'cache_check/init.pk_jt.pk'
     # check_diff(ours_weights_path, yx_weights_path)
 
