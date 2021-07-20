d9a93a1babc1402e74290f5246542859c3598228
cxjyxx_me <498731903@qq.com>
2021-07-20 15:22:40 +0800

    limit gt

--- python/jdet/models/networks/retinanet.py
+++ python/jdet/models/networks/retinanet.py
@@ -36,6 +36,21 @@ class RetinaNet(nn.Module):
             results: detections
             losses (dict): losses
         '''
+        # limit gt [-pi/4,pi*3/4] to [-pi/2,0) TODO: move to dataloader
+        for i in range(len(targets)):
+            gt_bbox = targets[i]["bboxes"].data#xywha
+            out_bbox = []
+            for j in range(gt_bbox.shape[0]):
+                box = gt_bbox[j]
+                x, y, w, h, a = box[0], box[1], box[2], box[3], box[4]
+                if (a >= 0):
+                    a -= np.pi
+                if (a < -np.pi / 2):
+                    a += np.pi / 2
+                    w, h = h, w
+                out_bbox.append(np.array([x, y, w, h, a])[np.newaxis, :])
+            out_bbox = np.concatenate(out_bbox, 0)
+            targets[i]["bboxes"] = jt.array(out_bbox)
         features = self.backbone(images)
         if self.neck:
             features = self.neck(features)
