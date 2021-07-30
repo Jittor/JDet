4e20a4dd977b3e18807622b201f02f55d3705a3f
cxjyxx_me <498731903@qq.com>
2021-07-30 16:28:15 +0800

    test_mode and default use cuda

--- projects/retinanet/run_net.py
+++ projects/retinanet/run_net.py
@@ -2,8 +2,9 @@ import argparse
 import os
 import jittor as jt
 from jdet.runner import Runner 
-from jdet.config import init_cfg
+from jdet.config import init_cfg, get_cfg
 import models
+import jdet
 
 def main():
     parser = argparse.ArgumentParser(description="Jittor Object Detection Training")
@@ -21,13 +22,13 @@ def main():
         type=str,
     )
     parser.add_argument(
-        "--use_cuda",
+        "--no_use_cuda",
         action='store_true'
     )
     
     args = parser.parse_args()
 
-    if args.use_cuda:
+    if not args.no_use_cuda:
         jt.flags.use_cuda=1
 
     assert args.task in ["train","val","test"],f"{args.task} not support, please choose [train,val,test]"
@@ -35,6 +36,11 @@ def main():
     if args.config_file:
         init_cfg(args.config_file)
     
+    cfg = get_cfg()
+    if (cfg.test_mode):
+        jt.set_global_seed(666)
+        jdet.utils.general.set_random_seed(666)
+
     runner = Runner()
 
     if args.task == "train":
