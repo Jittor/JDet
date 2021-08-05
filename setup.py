dd270ca3e819904c318bf825017b8dbe4afa97bf
li-xl <1905692338@qq.com>
2021-08-05 21:25:27 +0800

    update version

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup,find_packages
 import os
 path = os.path.join(os.path.dirname(__file__),"python")
+
+with open(os.path.join(path, "jdet/__init__.py"), "r", encoding='utf8') as fh:
+    for line in fh:
+        if line.startswith('__version__'):
+            version = line.split("'")[1]
+            break
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setup(
     name="jdet",
-    version="0.1",
+    version=version,
     author="Jittor Group",
     author_email="jittor@qq.com",
     description="Jittor Aerial Image Detection",
