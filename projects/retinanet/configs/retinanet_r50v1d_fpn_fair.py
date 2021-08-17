91d6213af34669b54300e7df1d22a17a37fb4afa
cxjyxx_me <498731903@qq.com>
2021-08-17 05:41:58 -0400

    fix fair

--- projects/retinanet/configs/retinanet_r50v1d_fpn_fair.py
+++ projects/retinanet/configs/retinanet_r50v1d_fpn_fair.py
@@ -85,6 +85,7 @@ dataset = dict(
     ),
     test = dict(
       type= "ImageDataset",
+      dataset_type="FAIR",
       images_dir= "/home/cxjyxx_me/workspace/JAD/datasets/FAIR/processed/test_600_150_1.0/images/",
       transforms= [
         dict(
