d6f92a09ec45c14fe20ef55d4de0b5039b7d86c4
li-xl <1905692338@qq.com>
2021-08-23 10:53:20 +0800

    add evaluation

--- python/jdet/runner/runner.py
+++ python/jdet/runner/runner.py
@@ -79,8 +79,7 @@ class Runner:
         self.logger.print_log("Start running")
         while not self.finish:
             self.train()
-            # TODO open evaluation
-            if False and check_interval(self.epoch,self.eval_interval):
+            if check_interval(self.epoch,self.eval_interval):
                 # TODO: need remove this
                 self.model.eval()
                 self.val()
