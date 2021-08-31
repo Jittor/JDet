b2483c4d907f409cc41cbd3f6c29361ce289fe5d
cxjyxx_me <498731903@qq.com>
2021-08-30 22:55:16 -0400

    fix

--- tools/test_models.py
+++ tools/test_models.py
@@ -2,15 +2,14 @@ import os
 tasks = ["faster_rcnn", "retinanet", "s2anet", "ssd"]
 
 if (not os.path.exists("test_datas.zip")):
-    os.system("wget https://cloud.tsinghua.edu.cn/f/63b1224dbfc6464495d5/?dl=1")
+    os.system("wget https://cloud.tsinghua.edu.cn/f/c790a39cfdd7453c9732/?dl=1")
     os.system("mv 'index.html?dl=1' test_datas.zip")
     os.system("unzip -o test_datas.zip")
     for task in tasks:
         src_path = os.path.join(os.path.abspath('.'), f"test_datas/test_datas_{task}")
         tar_path = os.path.join(os.path.abspath('.'), f"projects/{task}/test_datas_{task}")
         if (os.path.exists(tar_path) or os.path.islink(tar_path)):
-            print(f"rm {tar_path}")
-            os.system(f"rm {tar_path}")
+            os.system(f"rm -rf {tar_path}")
         os.system(f"ln -s {src_path} {tar_path}")
 for task in tasks:
     print(f"===============testing {task}==================")
