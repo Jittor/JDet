55bd14488ba484fdea47ae2f92d0fa81b3e942a8
CzzzzH <282492021@qq.com>
2021-10-06 14:10:35 +0800

    Add gliding to the test_models.py

--- tools/test_models.py
+++ tools/test_models.py
@@ -1,6 +1,5 @@
 import os
-tasks = ["gliding"]
-# tasks = ["roi_transformer", "faster_rcnn", "retinanet", "s2anet", "ssd", "gliding"]
+tasks = ["roi_transformer", "faster_rcnn", "retinanet", "s2anet", "ssd", "gliding"]
 zip_path = "https://cloud.tsinghua.edu.cn/f/5812c7ea8b594e92a433/?dl=1"
 
 if (not os.path.exists("test_datas.zip")):
