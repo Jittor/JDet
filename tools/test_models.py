9246f7481d97d4e5141281b153804facfdf2dc47
zytx121 <592267829@qq.com>
2022-11-16 17:00:55 +0800

    Update test_models.py

--- tools/test_models.py
+++ tools/test_models.py
@@ -1,7 +1,7 @@
 import os
 tasks = ["roi_transformer", "faster_rcnn", "retinanet", "s2anet", "ssd", "gliding", 
 "oriented_rcnn", "fcos", "yolo", "rotated_retinanet"]
-zip_path = "https://cloud.tsinghua.edu.cn/f/a5bcad6d48bc42a38c06/?dl=1"
+zip_path = "https://cloud.tsinghua.edu.cn/f/a541544812534186a44c/?dl=1"
 
 if (not os.path.exists("test_datas.zip")):
     os.system(f"wget {zip_path}")
