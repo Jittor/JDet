748d1a3ca0280eb19c1debc68213b9d04984f66b
Zhaohui Zheng <57825730+Zzh-tju@users.noreply.github.com>
2022-11-28 22:31:33 +0800

    Add files via upload

--- python/jdet/models/roi_heads/__init__.py
+++ python/jdet/models/roi_heads/__init__.py
@@ -2,6 +2,8 @@ from .rbbox_head import *
 from .convfc_rbbox_head import *
 from . import retina_head
 from . import rotated_retina_head
+from . import rotated_retina_distribution_head
+from . import ld_rotated_retina_head
 from . import s2anet_head
 from . import rpn_head
 from . import oriented_rpn_head
@@ -11,6 +13,5 @@ from . import gliding_head
 from . import ssd_head
 from . import fasterrcnn_head
 from . import fcos_head
-from . import kfiou_rotated_retina_head
 from . import h2rbox_head
 __all__ = []
