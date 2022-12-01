79d52c5db7f8cdf1ab3f64b8c13eaf870ce169d1
Zhaohui Zheng <57825730+Zzh-tju@users.noreply.github.com>
2022-12-01 19:57:54 +0800

    Update __init__.py

--- python/jdet/models/losses/__init__.py
+++ python/jdet/models/losses/__init__.py
@@ -3,6 +3,7 @@ from .focal_loss import FocalLoss
 from .cross_entropy_loss import CrossEntropyLoss
 from .l1_loss import L1Loss
 from .poly_iou_loss import PolyIoULoss
+from .kf_iou_loss import KFLoss
 from .gaussian_dist_loss import GDLoss
 from .gaussian_dist_loss_v1 import GDLoss_v1
 from .iou_loss import IoULoss
